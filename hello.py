ff84790adabc07332b1e36a550c167339154d6cf
Timothy Jaeryang Baek <tim@openwebui.com>
2025-01-27 23:18:13 -0800

    refac

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from llama-cpp-runner!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/llama_cpp_runner/__init__.py
@@ -0,0 +1,5 @@
+from llama_cpp_runner.main import LlamaCppServer
+
+
+def hello() -> str:
+    return "Hello from llama-cpp-runner!"
